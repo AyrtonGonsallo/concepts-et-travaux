c687040ba97e1f40c38948937375753527c3be75
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2024-04-24 17:30:55 +0100

    fin premiere journee

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,14 +3,9 @@ import { NgModule } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { fr_FR } from 'ng-zorro-antd/i18n';
-import { IconsProviderModule } from '../../icons-provider.module';
+import { SharedModule } from '../../shared/shared.module';
 @NgModule({
-  imports: [WelcomeRoutingModule,NzLayoutModule,
-    NzMenuModule,IconsProviderModule],
+  imports: [WelcomeRoutingModule,SharedModule ],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
