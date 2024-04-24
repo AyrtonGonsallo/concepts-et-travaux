1993864aacdff33982ac7e81203588ede23f1b3f
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2024-04-24 11:45:46 +0100

    second commit

--- src/app/pages/welcome/welcome.module.ts
+++ src/app/pages/welcome/welcome.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { WelcomeRoutingModule } from './welcome-routing.module';
 
 import { WelcomeComponent } from './welcome.component';
-
-
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { fr_FR } from 'ng-zorro-antd/i18n';
+import { IconsProviderModule } from '../../icons-provider.module';
 @NgModule({
-  imports: [WelcomeRoutingModule],
+  imports: [WelcomeRoutingModule,NzLayoutModule,
+    NzMenuModule,IconsProviderModule],
   declarations: [WelcomeComponent],
   exports: [WelcomeComponent]
 })
