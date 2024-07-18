0c2e5abcd5868b0b7add888acb30d60844cf04fa
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2024-07-18 11:33:26 +0100

    calculs manuels pose peinture, murs, sol + depose murs

--- src/app/Services/calcul-devis.service.ts
+++ src/app/Services/calcul-devis.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@angular/core';
 import { ApiConceptsEtTravauxService } from './api-concepts-et-travaux.service';
 import { DevisPiece } from '../Models/DevisPiece';
-import { DevisTache } from '../Models/DevisTache';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalculDevisService {
 
-  constructor(private devisService: ApiConceptsEtTravauxService, ) {
+  constructor(private userService: ApiConceptsEtTravauxService, ) {
+    this.userService.getTacheGeneraleById(2).subscribe((data) => {
+      this.tache_retirer_carrelage = data;
+    });
+    this.userService.getTacheGeneraleById(4).subscribe((data) => {
+      this.tache_retirer_papier = data;
+    });
+    this.userService.getTacheGeneraleById(1).subscribe((data) => {
+      this.tache_retirer_peinture = data;
+    });
+    this.userService.getTacheGeneraleById(3).subscribe((data) => {
+      this.tache_retirer_enduit = data;
+    });
+    this.userService.getTacheGeneraleById(6).subscribe((data) => {
+      this.tache_retirer_lambris = data;
+    });
+    this.userService.getTacheGeneraleById(5).subscribe((data) => {
+      this.tache_retirer_tissus = data;
+    });
   }
   devispiece:any
 
@@ -36,6 +53,9 @@ export class CalculDevisService {
     }
     else if(tacheid==8){
       prix=this.get_prix_tache_8(donnees_json)
+    }
+    else if(tacheid==14){
+      prix=this.get_prix_tache_14(donnees_json)
     }
     tache.Prix=prix
     return prix
@@ -126,6 +146,39 @@ get_prix_tache_5(donnees_json:any){
     prix+=has_pvc*prix_pvc
     prix+=prix_paquet_flottant_finition_bois*surface
     prix+=prix_parquet_flottant_finition_stratifiee*surface
+      
+    return prix
+  }
+  tache_retirer_carrelage:any
+  tache_retirer_papier:any
+  tache_retirer_enduit:any
+  tache_retirer_lambris:any
+  tache_retirer_peinture:any
+  tache_retirer_tissus:any
+  get_prix_tache_14(donnees_json:any){
+    
+    let prix=0
+
+    let dimensions = donnees_json["dimensions-depose-murs"].murs;
+    let gammes = donnees_json["gammes-produits-depose-murs"].murs;
+    let etat_surfaces = donnees_json["etat-surfaces-depose-murs"].murs;
+    let total=dimensions.length
+    for(let i=0;i<total;i++){
+      let surface=dimensions[i].surface
+      let has_carrelage=(gammes[i].carrelage)?1:0;
+      let has_papier=(gammes[i].papier)?1:0;
+      let has_enduit=(gammes[i].enduit)?1:0;
+      let has_peinture=(gammes[i].peinture)?1:0;
+      let has_lambris=(gammes[i].lambris)?1:0;
+      let has_tissus=(gammes[i].tissus)?1:0;
+
+      prix+=has_carrelage*surface*this.tache_retirer_carrelage.Prix
+      prix+=has_papier*surface*this.tache_retirer_papier.Prix
+      prix+=has_peinture*surface*this.tache_retirer_peinture.Prix
+      prix+=has_enduit*surface*this.tache_retirer_enduit.Prix
+      prix+=has_lambris*surface*this.tache_retirer_lambris.Prix
+      prix+=has_tissus*surface*this.tache_retirer_tissus.Prix
+    }
       
     return prix
   }
