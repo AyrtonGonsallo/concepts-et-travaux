ccf268fba91074b4310f675a80e45f337c95194d
AyrtonGonsallo <ayrtongonsallo444@gmail.com>
2024-12-12 15:27:48 +0100

    corrections logos, images et title

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,11 +2,11 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000', // URL de l'API locale
+   // apiUrl: 'http://localhost:3000', // URL de l'API locale
     //apiUrl: 'https://mala3315.odns.fr/api-concepts-et-travaux', 
-    //apiUrl: 'https://dev.homeren.fr/api-concepts-et-travaux', // URL de l'API sur le dev
+    apiUrl: 'https://dev.homeren.fr/api-concepts-et-travaux', // URL de l'API sur le dev
     //apiUrl: 'https://homeren.fr/api-concepts-et-travaux', // URL de l'API en prod
-    imagesUrl: 'https://homeren.fr/api-concepts-et-travaux/files/',
-    //imagesUrl: 'https://dev.homeren.fr/api-concepts-et-travaux/files/', // URL de l'API sur le dev
+    //imagesUrl: 'https://homeren.fr/api-concepts-et-travaux/files/',
+    imagesUrl: 'https://dev.homeren.fr/api-concepts-et-travaux/files/', // URL de l'API sur le dev
    
   };
